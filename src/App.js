8095b1944640b7c1c959b0f88b71eb834cb8bdf4
Justin Justin <jbry0723@gmail.com>
2020-11-05 20:08:25 -0500

    finished styling

--- src/App.js
+++ src/App.js
@@ -5,10 +5,22 @@ import moment from "moment"
 import TitleDateBar from './TitleDateBar'
 import ImgContainer from './ImgContainer'
 import ExplainationBox from './ExplainationBox'
+import styled from 'styled-components'
+import DateStamp from './DateStamp'
 
 const BASE_URL="https://api.nasa.gov/planetary/apod?"
 const currentDate=moment().format('YYYY-MM-DD')
 const nasaKey="gchmnzUhxntLeN5dKZdO7i22j2Zy2aZyZu8LiPiT"
+
+const StyledBody=styled.div`
+display:flex;
+flex-direction:column;
+align-items:center;
+background-color:${pr=>pr.theme.mainBackgroundColor};
+
+
+
+`
 
 
 function App() {
@@ -31,11 +43,13 @@ function App() {
 },[])
 
   return (
-    <div className="App">
+    <StyledBody>
      <TitleDateBar imgData={imgData} />
+    
      <ImgContainer imgData={imgData} />
+     <DateStamp imgData={imgData} />
      <ExplainationBox imgData={imgData} />
-    </div>
+    </StyledBody>
   );
 }
 
--- src/ImgContainer.js
+++ src/ImgContainer.js
@@ -1,12 +1,20 @@
 import React from "react";
+import styled,{keyframes} from 'styled-components'
+
+const ImgDiv=styled.div`
+padding: 0rem;
+border: 10px groove ${pr=>pr.theme.boxColor};
+margin-top:2rem;
+box-shadow:0px 3px #190a1c;
+`
 
 const ImgContainer=(props)=>{
     const {imgData}=props
 
     return (
-        <div>
+        <ImgDiv>
             <img src={imgData.url}></img>
-        </div>
+        </ImgDiv>
     )
 }
 
